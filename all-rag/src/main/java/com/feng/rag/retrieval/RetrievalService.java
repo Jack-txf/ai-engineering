6bdf57b70ddb0a874c62576abc8f3efc91e95013
jack_txf <762600503@qq.com>
2026-03-28 18:36:22 +0800

    feat(retrieval): 检索功能

---
+++ all-rag/src/main/java/com/feng/rag/retrieval/RetrievalService.java
@@ -0,0 +1,14 @@
+package com.feng.rag.retrieval;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Description: 检索服务
+ * @Author: txf
+ * @Date: 2026/3/28
+ */
+@Service
+@Slf4j
+public class RetrievalService {
+}
